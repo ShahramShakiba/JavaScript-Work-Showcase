5f2f7e4f19fb98eee96f1ceb6a9c08dbac5561d8
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-08 12:02:10 +0330

    Update functionality

--- Show-Hide-Password/index.js
+++ Show-Hide-Password/index.js
@@ -4,11 +4,11 @@ const toggle = document.querySelector('.toggle'),
 toggle.addEventListener('click', () => {
   if (input.type === 'password') {
     input.type = 'text';
-    toggle.classList.remove('uil-eye');
-    toggle.classList.add('uil-eye-slash');
+    toggle.classList.add('uil-eye');
+    toggle.classList.remove('uil-eye-slash');
   } else {
     input.type = 'password';
-    toggle.classList.remove('uil-eye-slash');
-    toggle.classList.add('uil-eye');
+    toggle.classList.add('uil-eye-slash');
+    toggle.classList.remove('uil-eye');
   }
 });
