29c106092bd4dd8a6e756323a2cfa0a7e4c1627d
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-13 21:31:21 +0330

    Update The Entire Of The Project (HTML, CSS, JS)

---
+++ Coming-Soon-Page/index.js
@@ -0,0 +1,37 @@
+/*================= newsLetter-toggle ===================*/
+const notifyBtn = document.querySelector('.notifyBtn');
+const closeBtn = document.querySelector('.close');
+
+const toggle = () => {
+  let newsLetter = document.querySelector('.newsLetter');
+
+  newsLetter.classList.toggle('active');
+};
+
+notifyBtn.addEventListener('click', toggle);
+closeBtn.addEventListener('click', toggle);
+
+/*================= Count-Down ===================*/
+const countDate = new Date(Date.UTC(2023, 11, 13, 19, 24, 35)).getTime();
+
+const updateTime = () => {
+  let now = new Date().getTime(),
+    gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 2;
+
+  let days = Math.floor(gap / day);
+  let hours = Math.floor((gap % day) / hour);
+  let minutes = Math.floor((gap % hour) / minute);
+  let seconds = Math.floor((gap % minute) / second);
+
+  document.getElementById('day').innerHTML = days;
+  document.getElementById('hour').innerHTML = hours;
+  document.getElementById('minute').innerHTML = minutes;
+  document.getElementById('second').innerHTML = seconds;
+};
+
+setInterval(updateTime, 1000);
