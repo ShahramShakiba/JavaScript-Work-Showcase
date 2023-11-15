b65964adfb849b043deafef34352b0a548485c50
Shahram Shakiba <ShahramShakiba88@gmail.com>
2023-11-15 21:25:11 +0330

    Implementing functionality to the Navigation

---
+++ Responsive-Navigation-Bar/index.js
@@ -0,0 +1,24 @@
+const navMenu = document.querySelector('#nav-menu'),
+  toggleMenu = document.querySelector('#nav-toggle'),
+  closeMenu = document.querySelector('#nav-close'),
+  navLink = document.querySelectorAll('.nav-link');
+
+toggleMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
+
+closeMenu.addEventListener('click', () => {
+  navMenu.classList.remove('show');
+});
+
+const linkAction = (e) => {
+  //active link
+  navLink.forEach((nav) => nav.classList.remove('active'));
+  
+  e.target.classList.add('active');
+  
+  //remove mobile menu
+  navMenu.classList.remove('show');
+}
+
+navLink.forEach((nav) => nav.addEventListener('click', linkAction));
